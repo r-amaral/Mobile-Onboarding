b4565fba9ded60ca7440d0dc53a174410b8fcf3f
Ruan Oliveira <ruanamaral40@gmail.com>
2022-07-26 16:59:25 -0300

    Projeto Finalizado

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,47 +1,43 @@
-const communitySection = document.querySelector('.community')
-const deliverySection = document.querySelector('.delivery')
-const toolsSection = document.querySelector('.tools')
+const communitySection = document.querySelector('.community');
+const deliverySection = document.querySelector('.delivery');
+const toolsSection = document.querySelector('.tools');
 
-const paginationItem = document.querySelectorAll('.pagination__item')
-const buttonNavigation = document.querySelectorAll('button')
+const buttonNavigation = document.querySelectorAll('button');
+const linkNavigation = document.querySelectorAll('a');
 
+const pagination = document.querySelectorAll(".pagination");
+const paginationItem = document.querySelectorAll('.pagination__item');
 
-const paginationCommunity = document.querySelector(".pagination--community");
-paginationCommunity.addEventListener("click", function (event) {
-
+pagination[0].addEventListener("click", function (event) {
     if (event.target == paginationItem[1] || event.target == buttonNavigation[0]) {
         communitySection.style.left = '-200vw';
         deliverySection.style.left = '0';
         toolsSection.style.left = '200vw';
     }
 
-    if (event.target == paginationItem[2] || event.target == buttonNavigation[1]) {
+    if (event.target == paginationItem[2] || event.target == linkNavigation[0]) {
         communitySection.style.left = '-400vw';
         deliverySection.style.left = '-200vw';
-        toolsSection.style.left = '0'
+        toolsSection.style.left = '0';
     }
 })
 
-const paginationDelivery = document.querySelector(".pagination--delivery");
-paginationDelivery.addEventListener("click", function (event) {
-
+pagination[1].addEventListener("click", function (event) {
     if (event.target == paginationItem[3]) {
         communitySection.style.left = '0vw';
-        deliverySection.style.left = '200vw'
+        deliverySection.style.left = '200vw';
         toolsSection.style.left = '400vw';
     }
 
-    if (event.target == paginationItem[5] || event.target == buttonNavigation[2] || event.target == buttonNavigation[3]) {
+    if (event.target == paginationItem[5] || event.target == buttonNavigation[1] || event.target == linkNavigation[1]) {
         communitySection.style.left = '-400vw';
-        deliverySection.style.left = '-200vw'
-        toolsSection.style.left = '0'
+        deliverySection.style.left = '-200vw';
+        toolsSection.style.left = '0';
     }
 })
 
-const paginationTools = document.querySelector(".pagination--tools");
-paginationTools.addEventListener("click", function (event) {
-
-    if (event.target == paginationItem[6] || event.target == buttonNavigation[4]) {
+pagination[2].addEventListener("click", function (event) {
+    if (event.target == paginationItem[6] || event.target == buttonNavigation[2]) {
         communitySection.style.left = '0vw';
         deliverySection.style.left = '200vw';
         toolsSection.style.left = '400vw';
