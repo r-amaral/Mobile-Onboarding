96df3d57a395227079c7d1b968095e201c986259
Ruan Oliveira <ruanamaral40@gmail.com>
2022-07-25 22:19:48 -0300

    Projeto finalizado com apenas uma pagina e paginação utilizando JavaScript

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,24 +1,54 @@
-const communityButton = document.getElementById('community')
-const deliveryButton = document.getElementById('delivery');
-const toolsButton = document.getElementById('tools');
+const communitySection = document.querySelector('.community')
+const deliverySection = document.querySelector('.delivery')
+const toolsSection = document.querySelector('.tools')
 
-const button = document.querySelectorAll('button')
+const paginationItem = document.querySelectorAll('.pagination__item')
+const buttonNavigation = document.querySelectorAll('button')
 
-button.forEach(button => {
 
-    button.addEventListener('click', () => {
+const paginationCommunity = document.querySelector(".pagination--community");
+paginationCommunity.addEventListener("click", function (event) {
 
-        if(button == communityButton)
-            window.open('../views/safe-delivery.html', '_self')
-        
-        if(button == deliveryButton) 
-            window.open('../views/buy-&-sell-tools.html', '_self')
-     
-        if(button.textContent == 'Skip')
-            window.open('../views/buy-&-sell-tools.html', '_self')
+    if (event.target == paginationItem[1] || event.target == buttonNavigation[0]) {
+        communitySection.style.left = '-200vw';
+        deliverySection.style.left = '0';
+        toolsSection.style.left = '200vw';
+    }
 
-        if(button == toolsButton)
-            window.open('../index.html', '_self')
-              
-    })
+    if (event.target == paginationItem[2] || event.target == buttonNavigation[1]) {
+        communitySection.style.left = '-400vw';
+        deliverySection.style.left = '-200vw';
+        toolsSection.style.left = '0'
+    }
+})
+
+const paginationDelivery = document.querySelector(".pagination--delivery");
+paginationDelivery.addEventListener("click", function (event) {
+
+    if (event.target == paginationItem[3]) {
+        communitySection.style.left = '0vw';
+        deliverySection.style.left = '200vw'
+        toolsSection.style.left = '400vw';
+    }
+
+    if (event.target == paginationItem[5] || event.target == buttonNavigation[2] || event.target == buttonNavigation[3]) {
+        communitySection.style.left = '-400vw';
+        deliverySection.style.left = '-200vw'
+        toolsSection.style.left = '0'
+    }
+})
+
+const paginationTools = document.querySelector(".pagination--tools");
+paginationTools.addEventListener("click", function (event) {
+
+    if (event.target == paginationItem[6] || event.target == buttonNavigation[4]) {
+        communitySection.style.left = '0vw';
+        deliverySection.style.left = '200vw';
+        toolsSection.style.left = '400vw';
+    }
+    if (event.target == paginationItem[7]) {
+        communitySection.style.left = '-200vw';
+        deliverySection.style.left = '0vw';
+        toolsSection.style.left = '200vw';
+    }
 })
